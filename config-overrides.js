3c10ae87481255aef9f68fed7dd1a81c2d4f5e78
Suresh Kumar Mukhiya <itsmeskm99@gmail.com>
2022-05-29 16:18:00 +0200

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -4,11 +4,11 @@ const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 const cspConfigPolicy = {
     'default-src': "'self'",
     'base-uri': "'self'",
-    'object-src': "'none'",
-    'script-src': ["'self'"],
-    'style-src': ["'self'"],
+    'object-src': "'self'",
     'img-src': ["'self'"],
     'font-src': ["'self'"],
+    'script-src': ["'unsafe-inline'", "'self'"],
+    'style-src': ["'unsafe-inline'", "'self'"]
 };
 
 function addCspHtmlWebpackPlugin(config) {
